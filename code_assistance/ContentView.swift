961d656add5df8b409d7a46f4e048e404b920f0b
Sarim <sarimk80@gmail.com>
2025-04-03 14:50:53 +0500

    Initial Commit

---
+++ code_assistance/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  code_assistance
+//
+//  Created by sarim khan on 03/04/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ code_assistance/code_assistanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  code_assistanceApp.swift
+//  code_assistance
+//
+//  Created by sarim khan on 03/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct code_assistanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
